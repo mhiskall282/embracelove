d96b76e99ec6acbef22c629eb8140597e210282c
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2024-12-30 23:59:51 +0000

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,15 +4,21 @@ import { Programs } from '../components/Programs';
 import { Testimonials } from '../components/Testimonials';
 import { Team } from '../components/Team';
 import { Blog } from '../components/Blog';
+import { UpcomingEvents } from '../components/UpcomingEvents';
+import { DonationTiers } from '../components/DonationTiers';
+import { NewsletterSignup } from '../components/NewsletterSignup';
 
 export const HomePage = () => {
   return (
     <>
       <Hero />
       <Programs />
+      <UpcomingEvents />
       <Testimonials />
+      <DonationTiers />
       <Team />
       <Blog />
+      <NewsletterSignup />
     </>
   );
 };
